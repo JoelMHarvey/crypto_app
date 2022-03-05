4b9ef76c9c7fc6e96b3324a47b1ed97b04781112
Joel M Harvey <joelharvey@me.com>
2022-03-05 20:32:11 +0900

    crypto news added

---
+++ src/services/CryptoNewsApi.js
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+  'x-rapidapi-key': '4fd2655bc0msh312ba6b8acc540ep1b7286jsn4cba7585f21f',
+}
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&count=${count}`
+        ),
+    }),
+  }),
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi
+
+/* 
+ url: 'https://bing-news-search1.p.rapidapi.com/news',
+  params: {safeSearch: 'Off', textFormat: 'Raw'},
+  headers: {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '4fd2655bc0msh312ba6b8acc540ep1b7286jsn4cba7585f21f'
+  }
+}; */
